408dd623574ed5f748c49c90fe68af120e9de096
EliasDFranco <eliasfranco20160055@gmail.com>
2025-01-09 18:17:26 -0300

    Agregando nuevas funciones y botones a inventarios

--- Utils/inventario.py
+++ Utils/inventario.py
@@ -16,3 +16,81 @@ class Inventario(tk.Frame):
         titulo = tk.Label(self, text="Inventarios", bg="#50C878", font="Arial 30 bold", anchor="center")
         titulo.pack()
         titulo.place(x= 5, y=0, width=1090, height=90)
+
+#09-01-2025 
+        frame2 = tk.Frame(self, bg="#BAFBEB", highlightthickness=1, highlightbackground="gray")
+        frame2.place(x=0 , y=100, width=1100, height=550)
+
+        labelframe = LabelFrame(frame2, text="Productos", font="sans 21 bold", bg="#BAFBEB")
+        labelframe.place(x=20, y=30, width=400, height=500)
+
+        lblnombre = Label(labelframe, text="Nombre: ", font="sans 14 bold", bg="#BAFBEB")
+        lblnombre.place(x=10, y=20)
+        self.nombre = ttk.Entry(labelframe, font="sans 14 bold")
+        self.nombre.place(x=140, y=20, width=240, height=40)
+
+        lblproveedor = Label(labelframe, text="Proveedor: ", font="sans 14 bold" , bg="#BAFBEB")
+        lblproveedor.place(x=10, y=80)
+        self.proveedor = ttk.Entry(labelframe, font="sans 14 bold")
+        self.proveedor.place(x=140, y=80, width=240, height=40)
+
+        lblprecio = Label(labelframe, text="Precio: ", font="sans 14 bold", bg="#BAFBEB" )
+        lblprecio.place(x=10, y=140)
+        self.precio = ttk.Entry(labelframe, font="sans 14 bold")
+        self.precio.place(x=140 , y=140 , width=240 , height=40)
+
+        lblcosto = Label(labelframe, text="Costo: ", font="sans 14 bold", bg="#BAFBEB")
+        lblcosto.place(x=10 , y=200)
+        self.costo = ttk.Entry(labelframe, font="sans 14 bold")
+        self.costo.place(x=140 , y=200 , width=240 , height=40)
+
+        lblstock = Label(labelframe, text="Stock: ", font="sans 14 bold", bg="#BAFBEB")
+        lblstock.place(x=10, y=260)
+        self.stock = ttk.Entry(labelframe, font="sans 14 bold")
+        self.stock.place(x=140, y=260, width=240, height=40)
+
+        boton_agregar = tk.Button(labelframe, text="Ingresa", font="sans 14 bold", bg="#22CBA6")
+        boton_agregar.place(x=80, y=340, width=240, height=40)
+
+        boton_editar = tk.Button(labelframe, text="Editar", font="sans 14 bold", bg="#22CBA6")
+        boton_editar.place(x=80, y=400, width=240 , height=40 )
+
+        #Creando tabla en la parte derecha
+        treFrame = Frame(frame2, bg="white")
+        treFrame.place(x=440, y=50, width=620, height=400)
+
+        scrol_y = ttk.Scrollbar(treFrame)
+        scrol_y.pack(side=RIGHT, fill=Y)
+
+        scrol_x = ttk.Scrollbar(treFrame, orient=HORIZONTAL)
+        scrol_x.pack(side=BOTTOM, fill=X)
+
+        self.tre = ttk.Treeview(treFrame, yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set, height=40,
+        columns=("ID", "PRODUCTO", "PROVEEDOR", "PRECIO", "COSTO", "STOCK"), show="headings")
+        self.tre.pack(expand=True, fill=BOTH)
+
+        scrol_y.config(command=self.tre.yview)
+        scrol_x.config(command=self.tre.xview)
+
+        self.tre.heading("ID", text="id")
+        self.tre.heading("PRODUCTO", text="Producto")
+        self.tre.heading("PROVEEDOR", text="Proveedor")
+        self.tre.heading("PRECIO", text="Precio")
+        self.tre.heading("COSTO", text="Costo")
+        self.tre.heading("STOCK", text="Stock")
+
+        self.tre.column("ID", width=70, anchor="center")
+        self.tre.column("PRODUCTO", width=100, anchor="center")
+        self.tre.column("PROVEEDOR", width=100, anchor="center")
+        self.tre.column("PRECIO", width=100, anchor="center")
+        self.tre.column("COSTO", width=100, anchor="center")
+        self.tre.column("STOCK", width=70, anchor="center")
+
+
+
+
+
+
+        
+
+
